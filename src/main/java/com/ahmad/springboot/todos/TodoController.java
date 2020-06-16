21827532a5faeeac4f13e40071a12f2dfed4a43a
Ahmad Abdo <ahmed.abdel-gaied@appspro-me.com>
2020-06-16 05:04:08 +0200

    Dependency to avoid restart application on each change

--- src/main/java/com/ahmad/springboot/todos/TodoController.java
+++ src/main/java/com/ahmad/springboot/todos/TodoController.java
@@ -11,17 +11,21 @@ import java.util.List;
 @RequestMapping(value = "/api/v1/todos")
 public class TodoController {
 
+    private List<Todo> data = Arrays.asList(
+            new Todo(1, "First todo", "this is my first task"),
+            new Todo(2, "Second todo", "this is my second task"),
+            new Todo(3, "Third todo", "this is my third task"),
+            new Todo(4, "Forth todo", "this is my forth task"),
+            new Todo(5, "Fifth todo", "this is my fifth task"),
+            new Todo(6, "Sixth todo", "this is my sixth task")
+    );
+
+
     // Will return the below as JSON formatted
     @GetMapping(value = {"", "/"})
     public List<Todo> listTodos() {
 
-        return Arrays.asList(
-                new Todo(1, "First todo", "this is my first task"),
-                new Todo(2, "Second todo", "this is my second task"),
-                new Todo(3, "Third todo", "this is my third task"),
-                new Todo(4, "Forth todo", "this is my forth task"),
-                new Todo(5, "Fifth todo", "this is my fifth task")
-                );
+        return data;
     }
 
 
