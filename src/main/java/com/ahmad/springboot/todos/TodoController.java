5919cb0506ba81f5f3f52b7e22f64e6141d3450e
Ahmad Abdo <ahmed.abdel-gaied@appspro-me.com>
2020-06-17 18:48:21 +0200

    Change static methods to Response Entity

--- src/main/java/com/ahmad/springboot/todos/TodoController.java
+++ src/main/java/com/ahmad/springboot/todos/TodoController.java
@@ -1,8 +1,11 @@
 package com.ahmad.springboot.todos;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.Arrays;
 import java.util.List;
 
@@ -16,13 +19,16 @@ public class TodoController {
 
     // Will return the below as JSON formatted
     @GetMapping(value = {"", "/"})
-    public List<Todo> getAllTodos() {
-        return todoService.findAll();
+    public ResponseEntity<List<Todo>> getAllTodos() {
+
+        List<Todo> result = todoService.findAll();
+        return new ResponseEntity<List<Todo>>(result, HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
-    public Todo getTodoById(@PathVariable String id) {
-        return todoService.getById(id);
+    public ResponseEntity<Todo> getTodoById(@PathVariable String id) {
+        Todo result = todoService.getById(id);
+        return new ResponseEntity<Todo>(result, HttpStatus.OK);
     }
 
     @PostMapping(value = {"", "/"})
@@ -33,12 +39,15 @@ public class TodoController {
     }
 
     @PutMapping(value = {"", "/"})
-    public Todo editTodo(@RequestBody Todo todo) {
-        return todoService.edit(todo);
+    public ResponseEntity<Todo> editTodo(@Valid @RequestBody Todo todo) {
+        Todo result = todoService.edit(todo);
+        return new ResponseEntity<>(result, HttpStatus.CREATED);
     }
 
     @DeleteMapping("/{id}")
-    public void deleteTodo(@PathVariable String id) {
+    public ResponseEntity<Void> deleteTodo(@PathVariable String id) {
+
         todoService.delete(id);
+        return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
     }
 }
